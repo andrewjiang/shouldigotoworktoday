3f66fd484c0cc5e12b02e6c0b9439b9c9e6854f0
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-02-13 11:37:44 -0500

    Mixpanel added

--- sigtw.js
+++ sigtw.js
@@ -10,32 +10,7 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
-
-  Template.northeast.rendered = function() {
-    $(document).ready(function(){
-      function initialize() {
-        var mapOptions = {
-          zoom: 6,
-          center: new google.maps.LatLng(39.8315166,-74.3260091),
-          mapTypeId: google.maps.MapTypeId.SATELLITE
-        };
-
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
-        var weatherLayer = new google.maps.weather.WeatherLayer({
-          temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
-        });
-        weatherLayer.setMap(map);
-
-        var cloudLayer = new google.maps.weather.CloudLayer();
-        cloudLayer.setMap(map);
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-    });
 };
-}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
